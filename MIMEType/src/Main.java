d0b92a4fb2d3ddcc763fe1c02362ab7cfa63c088
Sylvia Nikolova <silviaadvert@gmail.com>
2020-11-16 07:59:44 +0200

    Practice Strings, Hash tables, Loops, Conditions

--- MIMEType/src/Main.java
+++ MIMEType/src/Main.java
@@ -6,14 +6,14 @@ public class Main {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
 
-        int N = in.nextInt(); // Number of elements which make up the association table.
-        int Q = in.nextInt(); // Number Q of file names to be analyzed.
+        int N = in.nextInt();  
+        int Q = in.nextInt(); 
 
         Map<String, String> lookupTable = new HashMap<>();
 
         for (int i = 0; i < N; i++) {
-            String EXT = in.next(); // file extension
-            String MT = in.next(); // MIME type.
+            String EXT = in.next(); 
+            String MT = in.next(); 
 
             lookupTable.putIfAbsent(EXT.toLowerCase(), MT);
         }
@@ -21,8 +21,7 @@ public class Main {
         in.nextLine();
 
         for (int i = 0; i < Q; i++) {
-            String FNAME = in.nextLine(); // One file name per line.
-
+            String FNAME = in.nextLine();  
             String mimeType = FNAME.substring(FNAME.lastIndexOf('.') + 1).toLowerCase();
 
             if (!FNAME.contains(".") || !lookupTable.containsKey(mimeType)) {
@@ -31,12 +30,5 @@ public class Main {
                 System.out.println(lookupTable.get(mimeType));
             }
         }
-
-        // Write an answer using System.out.println()
-        // To debug: System.err.println("Debug messages...");
-
-
-        // For each of the Q filenames, display on a line the corresponding MIME type. If there is no corresponding type, then display UNKNOWN.
-//        System.out.println("UNKNOWN");
     }
 }
